7844a8901db83920839a9896874d3175b6e66d7e
tgaines <tgaines@tsys.com>
2020-07-17 08:59:51 -0400

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -9,9 +9,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(doubleTheNumber(3));
+        int[] myList = {1, 2, 3, 5, 6, 7, 8, 9};
+        System.out.println(add(myList));
     }
 
+
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+
+    }
+
+    public static int add(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];}
+        return sum;
     }
 }
